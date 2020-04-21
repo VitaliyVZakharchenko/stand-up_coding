01fda8b705e1fe7d841a76a33444a8e9d58b8863
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-04-21 11:36:06 +0300

    21.04.2020

--- 21.04.2020/task_1/index.js
+++ 21.04.2020/task_1/index.js
@@ -6,6 +6,10 @@
 function squareArr(arr) {
     const result = [];
 
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+
     for (let i = 0; i < arr.length; i += 1) {
         const square = (arr[i] * arr[i]);
         result.push(square);
@@ -13,7 +17,7 @@ function squareArr(arr) {
     return result;
 }
 
-const numbers = [4, 5, 3, 4, 5];
+const numbers = [4, 5, 3, 4, 5, 8];
 const result = squareArr(numbers);
 
 console.log(result);
