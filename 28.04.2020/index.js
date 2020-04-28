f3d7f84df9710cb6745181de606025136bcf5152
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-04-28 11:47:44 +0300

    28.04.2020

--- 28.04.2020/index.js
+++ 28.04.2020/index.js
@@ -1,18 +1,6 @@
 'use strict'
 const numbersList = [4.784, 6.9324, 36646.48];
 
-// function callback (acc, num) {
-//     console.log(acc, num);
-//     const newAcc = acc + num;
-    
-//     return newAcc;
-// }
-
-// const sum = numbersList.reduce(callback, 0);
-
-// console.log(sum);
-
-
 const getTotalPrice = arr => {
     const sum = arr.reduce((acc, num) => {
         return acc + num;
