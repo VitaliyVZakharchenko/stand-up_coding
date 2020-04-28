669fd7a6ed1582aff0b6c72d6a18afd8dbc4473b
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-04-28 11:44:27 +0300

    28.04.2020

--- 28.04.2020/index.js
+++ 28.04.2020/index.js
@@ -13,13 +13,10 @@ const numbersList = [4.784, 6.9324, 36646.48];
 // console.log(sum);
 
 
-const getTotalPrice = (arr) => {
-    const sum = arr.reduce(function callback(acc, num) {
+const getTotalPrice = arr => {
+    const sum = arr.reduce((acc, num) => {
         return acc + num;
-        // acc + num
-        }
-    )    
-    // console.log(sum);
+        }, 0)     
     return '$' + Math.floor(sum * 100) / 100;
 };
 
