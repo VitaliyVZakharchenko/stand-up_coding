de8a1a2b1f4a6e487f518cb4b0d8749473a64e39
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-04-28 11:50:36 +0300

    28.04.2020

--- task_28.04.2020/index.js
+++ task_28.04.2020/index.js
@@ -1,10 +1,11 @@
 'use strict'
+
 const numbersList = [4.784, 6.9324, 36646.48];
 
 const getTotalPrice = arr => {
     const sum = arr.reduce((acc, num) => {
         return acc + num;
-        }, 0)     
+        }, 0);   
     return '$' + Math.floor(sum * 100) / 100;
 };
 
