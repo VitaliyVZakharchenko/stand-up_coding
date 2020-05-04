7bc8cc738674cec3380d45710657cacf173dd6e4
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-05-04 13:00:20 +0300

    04.05.2020

---
+++ task_04.05.2020/task_1/index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const addPropertyV1 = (userData, userId) => {
+    userData.id = userId;
+    return userData;
+};
+
+const addPropertyV2 = (userData, userId) =>
+    Object.assign(userData, {id: userId});
+
+const addPropertyV3 = (userData, userId) =>
+    Object.assign({}, userData, {id: userId});
+
+const addPropertyV4 = (userData, userId) => {
+    return {...userData, id: userId};
+};
---
+++ task_04.05.2020/task_2/index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const mergeObjectsV1 = (obj1, obj2) =>
+    Object.assign({}, obj1, obj2);
+
+const mergeObjectsV2 = (obj1, obj2) =>
+    Object.assign({}, obj2, obj1);
+
+const mergeObjectsV3 = (obj1, obj2) => {
+    ({...obj1, ...obj2});
+};
+
+const mergeObjectsV4 = (obj1, obj2) => {
+    ({...obj2, ...obj1});
+};
+
+console.log(mergeObjectsV4(obj1, obj2));
