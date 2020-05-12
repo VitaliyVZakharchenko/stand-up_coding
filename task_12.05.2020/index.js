7f79432b4ccca5c774bfad54cbe48063a0fee13a
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-05-12 11:48:39 +0300

    SuC_12.05.2020

--- task_12.05.2020/index.js
+++ task_12.05.2020/index.js
@@ -4,5 +4,17 @@ export function makeCounter() {
     return function count() {
         return ++counter;    
     };
-    
 };
+
+const counter1 = makeCounter();
+const counter2 = makeCounter();
+
+console.log(counter1());
+console.log(counter1());
+console.log(counter1());
+console.log(counter1());
+
+console.log(counter2());
+console.log(counter2());
+console.log(counter2());
+console.log(counter2());
