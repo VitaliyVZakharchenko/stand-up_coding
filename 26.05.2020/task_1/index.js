5521b3798955adaf1890ce924129ae333f84315d
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-05-26 12:57:12 +0300

    lesson_21_hw_5

--- 26.05.2020/task_1/index.js
+++ 26.05.2020/task_1/index.js
@@ -5,21 +5,20 @@ export function finishList() {
     const el2 = document.querySelectorAll('li');
 
     const elOne = document.createElement('li');
-    elOne.textContent = 1;
+    elOne.textContent = '1';
     listEl.prepend(elOne);
-
     
     const specialEl = document.querySelector('.special');
     const el4 = document.createElement('li');
-    el4.textContent = 4;
+    el4.textContent = '4';
     specialEl.before(el4);
 
     const el6 = document.createElement('li');
-    el6.textContent = 6;
+    el6.textContent = '6';
     specialEl.after(el6);
 
     const el8 = document.createElement('li');
-    el8.textContent = 8;
+    el8.textContent = '8';
     listEl.append(el8);
 }
 
