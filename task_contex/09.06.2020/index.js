54a9f9df5778ea2131618e20ed6d82da13ac9de9
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-06-09 11:40:40 +0300

    09.06.2020

--- task_contex/09.06.2020/index.js
+++ task_contex/09.06.2020/index.js
@@ -1,56 +1,89 @@
 export const timer = {
-    minsPassed: 0,
     secondsPassed: 0,
-    intervalId: null,
-
-
-    //BROKEN
-    // startTimer() {
-    //     const adder = function () {
-    //         this.secondsPassed += 1;
-    //         console.log(this.secondsPassed);
-    //     };
-    
-    //FIX CONTENT
-    //     const newAdder = adder.bind(this);
-
-    //     setInterval(newAdder, 1000);
-    // },
+    minsPassed: 0,
+    timerId: null,
 
     startTimer() {
-        this.intervalId = setInterval(() => {
+        this.timerId = setInterval(() => {
             this.secondsPassed +=1;
-            
-            if (this.secondsPassed === 60) {
+            if(this.secondsPassed === 60) {
                 this.minsPassed +=1;
                 this.secondsPassed = 0;
             }
-            
-            console.log(this.secondsPassed);
         }, 1000);
+        // this.timerId = timerId;
     },
-        // console.log(this.intervalId);
-            
-    stopTimer() {
-        clearInterval(this.intervalId);
-        this.timerId = null;
-    },
-    
-    
     getTime() {
-        let secs = this.secondsPassed < 10
-            ? `0${this.secondsPassed}`
-            : this.secondsPassed;
-
+        let secs = this.secondsPassed < 10 ? `0${this.secondsPassed}` : this.secondsPassed;
         return `${this.minsPassed}:${secs}`;
     },
-
-    
+    stopTimer() {
+        clearInterval(this.timerId);
+        this.timerId = null;
+    },
     resetTimer() {
         this.stopTimer();
         this.secondsPassed = 0;
         this.minsPassed = 0;
     },
-};
+}
 
-// timer.startTimer();
+//let secondsPassed = 0;
+
+
+// export const timer = {
+//     minsPassed: 0,
+//     secondsPassed: 0,
+//     intervalId: null,
+
+
+//     //BROKEN
+//     // startTimer() {
+//     //     const adder = function () {
+//     //         this.secondsPassed += 1;
+//     //         console.log(this.secondsPassed);
+//     //     };
+    
+//     //FIX CONTENT
+//     //     const newAdder = adder.bind(this);
+
+//     //     setInterval(newAdder, 1000);
+//     // },
+
+//     startTimer() {
+//         this.intervalId = setInterval(() => {
+//             this.secondsPassed +=1;
+            
+//             if (this.secondsPassed === 60) {
+//                 this.minsPassed +=1;
+//                 this.secondsPassed = 0;
+//             }
+            
+//             console.log(this.secondsPassed);
+//         }, 1000);
+//     },
+//         // console.log(this.intervalId);
+            
+//     stopTimer() {
+//         clearInterval(this.intervalId);
+//         this.timerId = null;
+//     },
+    
+    
+//     getTime() {
+//         let secs = this.secondsPassed < 10
+//             ? `0${this.secondsPassed}`
+//             : this.secondsPassed;
+
+//         return `${this.minsPassed}:${secs}`;
+//     },
+
+    
+//     resetTimer() {
+//         this.stopTimer();
+//         this.secondsPassed = 0;
+//         this.minsPassed = 0;
+//     },
+// };
+
+// // timer.startTimer();
