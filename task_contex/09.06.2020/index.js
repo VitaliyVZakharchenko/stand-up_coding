78c1c4af5db2318ec938e4a71ea26ad001061d89
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-06-09 11:37:37 +0300

    09.06.2020

--- task_contex/09.06.2020/index.js
+++ task_contex/09.06.2020/index.js
@@ -1,36 +1,4 @@
-// export const timer = {
-//     secondsPassed: 0,
-//     minsPassed: 0,
-//     timerId: null,
-
-//     startTimer() {
-//         this.timerId = setInterval(() => {
-//             this.secondsPassed +=1;
-//             if(this.secondsPassed === 60) {
-//                 this.minsPassed +=1;
-//                 this.secondsPassed = 0;
-//             }
-//         }, 1000);
-//         // this.timerId = timerId;
-//     },
-//     getTime() {
-//         let secs = this.secondsPassed < 10 ? `0${this.secondsPassed}` : this.secondsPassed;
-//         return `${this.minsPassed}:${secs}`;
-//     },
-//     stopTimer() {
-//         clearInterval(this.timerId);
-//         this.timerId = null;
-//     },
-//     resetTimer() {
-//         this.stopTimer();
-//         this.secondsPassed = 0;
-//         this.minsPassed = 0;
-//     },
-// }
-
-//let secondsPassed = 0;
-
-const timer = {
+export const timer = {
     minsPassed: 0,
     secondsPassed: 0,
     intervalId: null,
