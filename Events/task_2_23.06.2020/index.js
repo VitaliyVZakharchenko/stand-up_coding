44911fb3d328cb6ce651cb3b230c37ab66c3393f
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-06-23 13:09:27 +0300

    lesson_31

--- Events/task_2_23.06.2020/index.js
+++ Events/task_2_23.06.2020/index.js
@@ -1,18 +1,9 @@
-'use strict';
+const buttons = document.querySelectorAll('.btn');
+console.log(buttons);
 
-const itemElem1 = document.querySelector('.list-item');
-
-function sayHi1(event) {
+const clickHandler = event => {
     console.log(event.target.textContent);
-}
+};
 
-itemElem1.addEventListener('click', sayHi1);
-
-
-const itemElem2 = document.querySelector('.list-item:nth-child(2)');
-itemElem2.addEventListener('click', sayHi1);
-
-
-
-const itemElem3 = document.querySelector('.list-item:last-child');
-itemElem3.addEventListener('click', sayHi1);
+[...buttons]
+    .forEach(btn => btn.addEventListener('click', clickHandler))
