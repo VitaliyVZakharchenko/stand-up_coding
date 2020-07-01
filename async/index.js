7d58db7fe657d54f70caeff09f72cabfcb886aa1
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-07-01 10:30:39 +0300

    lesson_34

--- async/index.js
+++ async/index.js
@@ -1,6 +1,6 @@
 const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1';
 
-async function getUserData(userId) {
+export async function getUserData(userId) {
     const response = await fetch(`${baseUrl}/users/${userId}`)
         console.log(response);
         
@@ -17,7 +17,7 @@ async function getUserData(userId) {
         // });
 }
 
-async function printUserData(userId) {
+export async function printUserData(userId) {
     try {
         const user = await getUserData(userId);
         console.log(user);
