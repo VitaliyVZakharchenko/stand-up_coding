10b65cdac445c07bab95a76eadb27665ae1b2f32
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-08-04 13:37:33 +0300

    filter

---
+++ arrays/03.08.2020/index.js
@@ -0,0 +1,121 @@
+// начальный массив
+const numbersList = [1, 2, 3, 4, 5];
+
+/* ==================================== */
+/* ===> мутирующие методы массивов <=== */
+/* ===> изменяют исходный массив <===== */
+/* ==================================== */
+
+
+/* pop - удалит из массива последний элемент и вернет его */
+
+// c помощью метода pop достаньте последний элемент массива и поместите его в переменную lastNumber
+// выведите эту переменную в консоль
+
+// ... code here
+
+const lastNumber = numbersList.pop();
+console.log(lastNumber);
+
+
+
+/* push - добавит элемент в конец массива */
+
+// c помощью метода push добавьте число 6 в конец массива
+// выведите numbersList в консоль
+
+// ... code here
+numbersList.push(6);
+console.log(numbersList);
+
+/* shift - удалит из массива первый элемент и вернет его */
+
+// c помощью метода shift достаньте первый элемент массива и поместите его в переменную firstNumber
+// выведите эту переменную в консоль
+
+// ... code here
+
+const firstNumber = numbersList.shift();
+console.log(firstNumber);
+
+/* unshift - добавит элемент в начало массива */
+
+// c помощью метода unshift добавьте число 0 в начало массива
+// выведите numbersList в консоль
+
+// ... code here
+numbersList.unshift();
+console.log(numbersList);
+
+
+const result = numbersList.sort((a, b) => b - a); /* мутирует исходный массив */
+console.log(result);
+
+
+
+/* ======================================= */
+/* ===> НЕ мутирующие методы массивов <=== */
+/* ===> НЕ изменяют исходный массив <===== */
+/* ======================================= */
+
+//algo
+//1.
+//2.
+//3.
+
+//input: number, number, number
+//output: string
+function sum(a, b, c) {
+    const sum = a + b + c;
+    return 'sum is' + sum;
+}
+
+/* метод map */
+/* const newArr = arr.map(callback) - создает и возвращает новый массив. Длина нового и исходного массивов равны */
+/* ф-ция callback запустится по очереди для каждого элемента начального массива arr */
+/* элементы массива newArr - это результат вызова callback для каждого элемента arr */
+/* newArr[i] = callback(arr[i]) */
+
+// c помощью метода map создайте новый массив squaredNumbers, в котором будут квадраты чисел из numbersList
+// выведите squaredNumbers в консоль
+
+// ... code here
+
+
+// const callbackFunc = number => {
+//     return number * number;
+// }
+
+
+//for map
+//input: function
+//output: array
+const squaredNumbers = numbersList.map(num => num * num);
+console.log(squaredNumbers);
+
+
+/* метод filter */
+/* const newArr = arr.filter(callback) - создает и возвращает новый отфильтрованный массив. */
+/* ф-ция callback запустится по очереди для каждого элемента начального массива arr */
+/* в newArr попадут только те элементы arr, для которых callback(arr[i]) вернет true */
+/* callback(arr[i]) === true -> элемент будет добавлен в финальный массив  */
+/* callback(arr[i]) === false -> элемент НЕ будет добавлен в финальный массив  */
+
+// c помощью метода filter создайте новый массив evenNumbers, в котором только четные числа из numbersList
+// выведите evenNumbers в консоль
+
+// ... code here
+
+const evenNumbers = numbersList.filter(num => {
+    return num > 50;
+});
+
+
+/* метод find */
+/* const arrElement = arr.filter(callback) - вернет первый элемент массива arr, который удовлетворяет условию в callback. */
+/* ф-ция callback запустится по очереди для каждого элемента начального массива arr */
+/* если callback(arr[i]) вернет true, то filter прекратит поиск и вернет этот элемент */
+
+// c помощью метода find найдите первое нечетное число в numbersList и выведите его в консоль
+
+// ... code here
---
+++ arrays/task_04.08.20/index.js
@@ -0,0 +1,36 @@
+//algo
+//1. create empty array
+//2. iterate array & call callback
+//3.
+
+//EXAMPLE
+//input: [10, 30, 1, 8]
+//output: [30]
+
+
+//input: array, function
+//output: array
+
+//callback
+//input:
+//utput:
+function filterArray(arr, callback) {
+    const result = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (callback(arr[i], i, arr)) {
+            result.push(arr[i]);
+        }
+    }
+
+    return result;
+}
+
+let array = [10, 30, 1, 8, 100];
+
+const callback = (element) => {
+    const res = element > 20;
+    return res;
+}
+
+console.log(filterArray(array, callback));
