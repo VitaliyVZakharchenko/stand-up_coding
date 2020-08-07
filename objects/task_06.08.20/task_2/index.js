b628e23e732901f555eca8020093a1ef0354b4e6
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-08-07 14:39:02 +0300

    Tools_lesson_3_task_2

--- objects/task_06.08.20/task_2/index.js
+++ objects/task_06.08.20/task_2/index.js
@@ -6,34 +6,34 @@
 //input: array, array
 //output: object
 
-// const buildObject = (keysList, valuesList) => {
-//     keysList.reduce((acc, key, index) => {
-//         return {...acc, [key]: valuesList[index]};
-//     }, {});
-// };
+const buildObject = (keysList, valuesList) => {
+    keysList.reduce((acc, key, index) => {
+        return {...acc, [key]: valuesList[index]};
+    }, {});
+};
 
 // const keys = ['name', 'address', 'age'];
 // const values = ['Bob', 'Ukraine', 34];
 
 // console.log(buildObject(keys, values));
 
-const buildObject = (keysList, valueList) => {
+// const buildObject = (keysList, valueList) => {
     
-        keysList.reduce((accamulator, key, index) => {
+//         keysList.reduce((accamulator, key, index) => {
         
-        console.log(accamulator);
-        console.log(key);
-        console.log(valueList[index]);
+//         console.log(accamulator);
+//         console.log(key);
+//         console.log(valueList[index]);
         
-        const res = { ...accamulator, [key]: valueList[index] };
+//         const res = { ...accamulator, [key]: valueList[index] };
         
-        return res;
-    }, {})
+//         return res;
+//     }, {})
 
-};
+// };
 
 
-const keys = ['name', 'age'];
-const values = ['Bob', '34'];
+// const keys = ['name', 'age'];
+// const values = ['Bob', '34'];
 
-buildObject(keys, values);
+// buildObject(keys, values);
