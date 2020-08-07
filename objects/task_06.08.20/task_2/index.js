99383c3975106bf6828d13e18774b1390ca13dfe
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-08-07 14:45:12 +0300

    Tools_lesson_3_task_2

--- objects/task_06.08.20/task_2/index.js
+++ objects/task_06.08.20/task_2/index.js
@@ -7,15 +7,15 @@
 //output: object
 
 const buildObject = (keysList, valuesList) => {
-    keysList.reduce((acc, key, index) => {
+    return keysList.reduce((acc, key, index) => {
         return {...acc, [key]: valuesList[index]};
     }, {});
 };
 
-// const keys = ['name', 'address', 'age'];
-// const values = ['Bob', 'Ukraine', 34];
+const keys = ['name', 'address', 'age'];
+const values = ['Bob', 'Ukraine', 34];
 
-// console.log(buildObject(keys, values));
+console.log(buildObject(keys, values));
 
 // const buildObject = (keysList, valueList) => {
     
