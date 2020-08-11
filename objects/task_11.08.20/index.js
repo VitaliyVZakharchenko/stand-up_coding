05718eaae711db384b4cf58123893ac5e333f3bd
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-08-11 11:35:44 +0300

    Tools_lesson_4_task_1

--- objects/task_11.08.20/index.js
+++ objects/task_11.08.20/index.js
@@ -6,23 +6,23 @@
 //2.
 //3.
 
-const markAdmins = (users, adminsIds) => {
-    return users.map(user => {
-        return { ...user, isAdmin: adminsIds.includes(user.id) };
-    });
-}
+// const markAdmins = (users, adminsIds) => {
+//     return users.map(user => {
+//         return { ...user, isAdmin: adminsIds.includes(user.id) };
+//     });
+// }
 
 const markAdmins = (users, adminsIds) =>
     users.map(user =>
         ({ ...user, isAdmin: adminsIds.includes(user.id) }));
 
-const adminIds = ['3'];
+// const adminIds = ['3'];
 
-const users = [
-    {id: '1', name: 'Bob'},
-    {id: '2', name: 'Tom'},
-    {id: '3', name: 'Sam'},
-    {id: '4', name: 'Tad'},
-];
+// const users = [
+//     {id: '1', name: 'Bob'},
+//     {id: '2', name: 'Tom'},
+//     {id: '3', name: 'Sam'},
+//     {id: '4', name: 'Tad'},
+// ];
 
-console.log(markAdmins(users, adminIds));
+// console.log(markAdmins(users, adminIds));
