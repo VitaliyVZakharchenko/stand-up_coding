498c2fe894f4d7857abcbb0e38202c0320c917b6
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-08-26 11:31:43 +0300

    React_lesson_4_hw_1

--- classes/task_1/index.js
+++ classes/task_1/index.js
@@ -1,27 +1,27 @@
-class Sportsman {
-    constructor(name) {
-        this.name = name;
-    }
+// class Sportsman {
+//     constructor(name) {
+//         this.name = name;
+//     }
 
-    run() {
-        console.log(`${this.name} is running`);
-    }
-}
+//     run() {
+//         console.log(`${this.name} is running`);
+//     }
+// }
 
-class Swimmer extends Sportsman {
-    constructor(name, style) {
-        super(name);
-        this.style = style;
-    }
+// class Swimmer extends Sportsman {
+//     constructor(name, style) {
+//         super(name);
+//         this.style = style;
+//     }
 
-    swim() {
-        console.log(`${this.name} is swimming ${this.style}`);
-    }
+//     swim() {
+//         console.log(`${this.name} is swimming ${this.style}`);
+//     }
 
-    test() {
-        console.log(this);
-    }
-}
+//     test() {
+//         console.log(this);
+//     }
+// }
 
 // const spotsman = new Sportsman('Den');
 // console.log(spotsman);
@@ -33,29 +33,29 @@ class Swimmer extends Sportsman {
 // const spotsman2 = new Sportsman('Ann');
 // spotsman2.run();
 
-const swimmer1 = new Swimmer('Gim', 'test style');
-console.log(swimmer1);
-swimmer1.test();
-swimmer1.swim();
+// const swimmer1 = new Swimmer('Gim', 'test style');
+// console.log(swimmer1);
+// swimmer1.test();
+// swimmer1.swim();
 
 
-// export class Sportsman {
-//     constructor(name) {
-//         this.name = name;
-//     }
+export class Sportsman {
+    constructor(name) {
+        this.name = name;
+    }
 
-//     run() {
-//         console.log(`${this.name} is running`);
-//     }
-// }
+    run() {
+        console.log(`${this.name} is running`);
+    }
+}
 
-// export class Swimmer extends Sportsman {
-//     constructor(name, style) {
-//         super(name);
-//         this.style = style;
-//     }
+export class Swimmer extends Sportsman {
+    constructor(name, style) {
+        super(name);
+        this.style = style;
+    }
 
-//     swim() {
-//         console.log(`${this.name} is swimming ${this.style}`);
-//     }
-// }
+    swim() {
+        console.log(`${this.name} is swimming ${this.style}`);
+    }
+}
